5527c0cded5682765c9580c20d04ca7e0c4ea545
Tobias Foresti <tobi.foresti@gmail.com>
2022-05-23 14:24:35 +0200

    edit pylint settings

--- src/template.py
+++ src/template.py
@@ -4,6 +4,8 @@
 """
 
 import argparse
+import os
+from logging import WARNING
 
 
 class CmdColors:
@@ -15,21 +17,8 @@ class CmdColors:
 
 
 def get_input_args():
-    """get the input arguments from commandline
-
-    Returns:
-        tuple: containing the input arguments
-    """
     my_parser = argparse.ArgumentParser()
-    my_parser.add_argument(
-        "-i",
-        "--input",
-        metavar="string",
-        action="store",
-        type=str,
-        required=True,
-        help="string to print to prompt",
-    )
+    my_parser.add_argument("-i", "--input", metavar="string", action="store", type=str, required=True, help="string to print to prompt")
     my_parser.add_argument("-d", "--dashes", action="store_true", help="prettify output with dashes")
     my_parser.add_argument("-s", "--stars", action="store_true", help="prettify output with stars")
 
@@ -47,15 +36,16 @@ def print_message_to_prompt(input_message, dashes=None, stars=None):
     """
     if not input_message:
         raise ValueError(f"{CmdColors.FAIL}ERROR: you need an input!{CmdColors.ENDC}")
+    os.system("")
     if dashes:
-        print(f"{'-'*42}\n")
+        print(f"{CmdColors.WARNING}{'-'*42}\n{CmdColors.ENDC}")
     if stars:
-        print(f"{'*'*42}\n")
+        print(f"{CmdColors.FAIL}{'*'*42}\n{CmdColors.ENDC}")
     print(input_message)
     if dashes:
-        print(f"\n{'-'*42}")
+        print(f"{CmdColors.WARNING}{'-'*42}\n{CmdColors.ENDC}")
     if stars:
-        print(f"\n{'*'*42}")
+        print(f"{CmdColors.FAIL}{'*'*42}\n{CmdColors.ENDC}")
 
 
 if __name__ == "__main__":
